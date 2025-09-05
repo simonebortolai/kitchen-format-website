f946d323b5e726e75926ef9004906d18b62fe3b4
Simone Bortolai <simone.bortolai@t3lab.it>
2025-09-05 11:47:10 +0200

    Add basePath for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/kitchen-format-website',
+  assetPrefix: '/kitchen-format-website',
   images: {
     unoptimized: true
   }
